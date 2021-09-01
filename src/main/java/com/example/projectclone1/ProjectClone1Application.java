112110624d39352869163279423efa4b99ef8bb1
Joonghyun <tbvjqlqlqlr@naver.com>
2021-09-01 14:53:03 +0900

    2021_08_31_FoodListProject(1) - 기본 프론트 뷰 검색 섹션 완성 - Controller 파일 생성

---
+++ src/main/java/com/example/projectclone1/ProjectClone1Application.java
@@ -0,0 +1,13 @@
+package com.example.projectclone1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectClone1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectClone1Application.class, args);
+    }
+
+}
